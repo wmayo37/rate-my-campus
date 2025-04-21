a14ee7e71fb9540c8965adc86c1a3b6d67dcadf8
Will Mayo <wm128@duke.edu>
2025-04-21 19:27:07 -0400

    sign in setup

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/User");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://wmayo37:CollegeStuff37!@campusreviews.jth7ixi.mongodb.net/users"
+);
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  UserModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success!");
+      } else {
+        res.json("The password was incorrect :(");
+      }
+    } else {
+      res.json("The user you input does not exist :(");
+    }
+  });
+});
+
+app.post("/register", (req, res) => {
+  UserModel.create(req.body)
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log("server is running");
+});
