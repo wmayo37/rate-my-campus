c0d765b5b4568c0b9ba2a47b96b1eca74fee51cd
Will Mayo <wm128@duke.edu>
2025-04-21 12:18:44 -0400

    added sample database

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://duke.edu/" target="_blank">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/5352/5352118.png"
+            className="logo"
+            alt="Campus logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
