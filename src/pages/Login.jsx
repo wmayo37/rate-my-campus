c13c03fc7184bca9d0b676d81c77421f1e13d7c0
Will Mayo <wm128@duke.edu>
2025-04-21 19:27:27 -0400

    user auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  return <h2>Login</h2>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001/login", { email, password })
+      .then((result) => {
+        console.log(result);
+        if (result.data === "Success!") {
+          navigate("/home");
+        } else {
+          alert(result.data);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Log In</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              autoComplete="off"
+              name="email"
+              className="form-control rounded-0"
+              onChange={(event) => setEmail(event.target.value)}
+            ></input>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              autoComplete="off"
+              name="password"
+              className="form-control rounded-0"
+              onChange={(event) => setPassword(event.target.value)}
+            ></input>
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Log In
+          </button>
+          <Link
+            to="/register"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+          >
+            Sign Up
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
-  return <h2>Signup</h2>;
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001/register", { name, email, password })
+      .then((result) => {
+        console.log(result);
+        navigate("/login");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              autoComplete="off"
+              name="name"
+              className="form-control rounded-0"
+              onChange={(event) => setName(event.target.value)}
+            ></input>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              autoComplete="off"
+              name="email"
+              className="form-control rounded-0"
+              onChange={(event) => setEmail(event.target.value)}
+            ></input>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              autoComplete="off"
+              name="password"
+              className="form-control rounded-0"
+              onChange={(event) => setPassword(event.target.value)}
+            ></input>
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Register
+          </button>
+          <p>Already have an account?</p>
+          <Link
+            to="/login"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+          >
+            Login
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
